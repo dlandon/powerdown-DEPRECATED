5b063590124a115499ad45b8871f0b7fb8f92671
dlandon <dlandon@cinci.rr.com>
2016-02-14 11:52:12 -0500

    Release 2016.02.14a.
    Fix update plugin issue.

--- source/powerdown.emhttp/BuildPowerdown.sh
+++ source/powerdown.emhttp/BuildPowerdown.sh
@@ -2,6 +2,6 @@
 # Make powerdown distribution
 #
 # Build the tgz.
-tar -cvzf powerdown-2016.02.14.tgz powerdown/
+tar -cvzf powerdown-2016.02.14a.tgz powerdown/
 
-md5sum powerdown-2016.02.14.tgz > MD5
+md5sum powerdown-2016.02.14a.tgz > MD5
