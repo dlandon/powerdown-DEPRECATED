4b01381a04996fbfebb873dc69cf8b4f418c65f0
dlandon <dlandon@cinci.rr.com>
2016-02-14 07:23:50 -0500

    Revert "Release 2016.02.16."
    This reverts commit ff59c7723ffe2bdb6f141cfcb4ed8b5e03c13cd2.

--- source/powerdown.emhttp/BuildPowerdown.sh
+++ source/powerdown.emhttp/BuildPowerdown.sh
@@ -2,6 +2,4 @@
 # Make powerdown distribution
 #
 # Build the tgz.
-tar -cvzf powerdown-2016.02.14.tgz powerdown/
-
-md5sum powerdown-2016.02.14.tgz > MD5
+tar -cvzf powerdown-2015.09.06.tgz powerdown/
