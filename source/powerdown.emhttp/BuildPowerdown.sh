fbe9770d2e7c4815c14127bdc5022034030b1e18
dlandon <dlandon@cinci.rr.com>
2016-09-06 17:29:29 -0400

    Revert "Release 2016.09.06"
    This reverts commit a0acaa650556161a591873e60cde98a5c95d231f.

--- source/powerdown.emhttp/BuildPowerdown.sh
+++ source/powerdown.emhttp/BuildPowerdown.sh
@@ -2,6 +2,6 @@
 # Make powerdown distribution
 #
 # Build the tgz.
-tar -cvzf powerdown-2016.09.06.tgz powerdown/
+tar -cvzf powerdown-2016.03.26.tgz powerdown/
 
-md5sum powerdown-2016.09.06.tgz > MD5
+md5sum powerdown-2016.03.26.tgz > MD5
