af5aa835d722265f4a099018665c27c6d3c5c653
dlandon <dlandon@cinci.rr.com>
2016-09-10 15:04:06 -0400

    Release 2016.09.10
    More v6.2 changes and some general cleanup.

--- source/powerdown.emhttp/BuildPowerdown.sh
+++ source/powerdown.emhttp/BuildPowerdown.sh
@@ -2,6 +2,6 @@
 # Make powerdown distribution
 #
 # Build the tgz.
-tar -cvzf powerdown-2016.09.06.tgz powerdown/
+tar -cvzf powerdown-2016.09.10.tgz powerdown/
 
-md5sum powerdown-2016.09.06.tgz > MD5
+md5sum powerdown-2016.09.10.tgz > MD5
